c84bab0a791ec74da1d6efbaab2dec086ad61df7
jazicorn <40859840+jazicorn@users.noreply.github.com>
2022-10-07 19:07:30 -0400

    used prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express, {Request,Response,Application} from 'express';
+import express, { Request, Response, Application } from 'express';
 
-const app:Application = express();
+const app: Application = express();
 
 const PORT = 8000;
 
-app.get("/", (req:Request, res:Response):void => {
-  res.send("Hello Typescript with Node.js!")
+app.get('/', (req: Request, res: Response): void => {
+    res.send('Hello Typescript with Node.js!');
 });
 
-app.listen(PORT, ():void => {
-  console.log(`Server Running here 👉 https://localhost:${PORT}`);
+app.listen(PORT, (): void => {
+    console.log(`Server Running here 👉 https://localhost:${PORT}`);
 });
