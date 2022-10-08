be0ed6b04ad8f6aca8619638bdf0d70f20244b89
jazicorn <40859840+jazicorn@users.noreply.github.com>
2022-10-07 21:53:42 -0400

    added routes folder

---
+++ src/routes/index.ts
@@ -0,0 +1,17 @@
+// import express, { Request, Response, Application } from 'express';
+// const app: Application = express();
+
+// database connection
+//import db from '../db';
+
+/**
+ * Example of Implementation:
+ * app.get('/:id', (req, res, next) => {
+  db.query('SELECT * FROM users WHERE id = $1', [req.params.id], (err, result) => {
+    if (err) {
+      return next(err)
+    }
+    res.send(result.rows[0])
+  })
+})
+*/
